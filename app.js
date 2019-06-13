4584b7e8be6248263b04c539e44140221546416d
Lauren Eggers <neyabenz@gmail.com>
2019-06-12 20:51:32 -0400

    callback on post, begin setup for information

--- app.js
+++ app.js
@@ -14,48 +14,58 @@ app.set('view engine', 'ejs');
 
 const campsSchema = new mongoose.Schema ({
     name: String,
-    img: String
+    img: String,
+    desc: String
 });
 const Campground = mongoose.model("Campground", campsSchema);
-
-// Campground.create(
-//     {
-//         name: 'fitch', img: 'https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg'
-//     }, (err, camp) =>{
-//         if(err){
-//             console.log(err);
-//         } else {
-//             console.log(camp, "added")
-//         }
-//     }
-// );
 
 app.get('/', (req, res) => {
     res.render('landing');
 });
 
-app.get('/camps', (req, res) => {
-
-    //get all campgrounds from db
+app.get('/camps', (req, res) => { 
     Campground.find({}, (err, campgrounds) => {
         if(err) {
-            console.log("error populating camps from mongodb", err)
+            console.log("error populating campgrounds from mongodb", err)
         } else {
             res.render('camps', {campgrounds: campgrounds})
         }
     });
 });
 
+// Campground.create({
+//     name: 'White Mountains',
+//     img: 'https://farm8.staticflickr.com/7285/8737935921_47343b7a5d.jpg',
+//     desc: 'Lots of great hiking and fishing. Potable water and grills available. It gets cold at night, bring enough firewood or warm sleeping bags.'
+// }, (err, camp)=> {
+//     if(err){
+//         console.log("error")
+//     } else {
+//         console.log("New one added", camp)
+//     }
+// })
+
 app.post('/camps', (req, res) => {
     Campground.create({
         name: req.body.name,
         img: req.body.image
-    });
+    }, (err, camp) => {
+        if(err) {
+            console.log("Error posting to DB", err)
+        } else {
+            console.log("Added to DB", camp)
+        }
+    })
     res.redirect('/camps');
 });
 
 app.get('/camps/new', (req, res) => {
     res.render('newCamp.ejs');
+});
+
+//get request for /camps/:id to show further info on individual camps
+app.get('/camps/:id', (req, res) => {
+    res.send("This will be info about requested camp")
 });
 
 app.get('*', (req, res) => {
