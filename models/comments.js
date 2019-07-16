14ec0d6ad677ee0c475c8d7eaa22c54ab4cdf71c
Lauren Eggers <neyabenz@gmail.com>
2019-07-16 13:00:41 -0400

    setup for comment card rating/date

--- models/comments.js
+++ models/comments.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
     text: String,
-    author: String
+    author: String,
+    rating: Number,
+    created: {type: Date, default: Date.now}
 });
 
 const Comment = mongoose.model('Comment', commentSchema);
