3bd4541563b9d24e3ae4cf1c6c85f1e5d8c2effe
Lauren Eggers <neyabenz@gmail.com>
2019-08-26 17:48:49 -0400

    required comments & campground to fix undef err

--- middleware/index.js
+++ middleware/index.js
@@ -1,20 +1,26 @@
+const Comment = require('../models/comments'),
+    Campground = require('../models/campgrounds');
+
 const middlewareObj = {};
 
-middlewareObj.authorizedUpdate = (res, id, label, body, redirect ='') => {
+middlewareObj.authorizedUpdate = (req, res, id, label, body, redirect ='') => {
+    console.log(label);
     label.findByIdAndUpdate(id,  body, (err) => {
         if (err) {
             console.log(`You are authorized to do that, but there was an error`, err);
         } else {
+            req.flash('success', 'Updated');
             res.redirect(`/camps/${redirect}`);
         }
     });
 };
 
-middlewareObj.authorizedDelete = (res, id, label, redirect = '') => {
+middlewareObj.authorizedDelete = (req, res, id, label, redirect = '') => {
     label.findByIdAndDelete(id, (err) => {
         if (err){
             res.send(`You have proper authorization, but there was an error`, err);
         } else {
+            req.flash('success', 'Removed');
             res.redirect(`/camps/${redirect}`);
         }
     });
@@ -24,7 +30,7 @@ middlewareObj.loginCheck = (req, res, next) => {
     if(req.isAuthenticated()){
         return next();
     } else {
-        req.flash('error', 'please login');
+        req.flash('error', `You must be logged in to do that! Please <a href="/login">Login</a> or <a href="register">Register</a>.`);
         res.redirect('/login');
     }
 };
