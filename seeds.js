a6ab799a480099d773833fa6200074a52fd8d3dc
Lauren Eggers <neyabenz@gmail.com>
2019-08-27 15:37:03 -0400

    dynamic pricing

--- seeds.js
+++ seeds.js
@@ -6,7 +6,7 @@ const data = [
     {
         name: 'Clouds Rest',
         img: 'https://farm9.staticflickr.com/8471/8137270056_21d5be6f52.jpg',
-        price: '$10.00',
+        price: '10.00',
         desc: 'Maroon gun piracy hands driver fore gabion scurvy gangway measured fer yer chains. Red ensign black spot lugsail Barbary Coast spanker galleon plunder broadside no prey, no pay heave to. Trysail splice the main brace doubloon Davy Jones Locker cog Nelsons folly ahoy league spike hands.',
         author: {
             id: "588c2e092403d111454fff76",
@@ -16,7 +16,7 @@ const data = [
     {
         name: 'Crystal Lake',
         img: 'https://farm2.staticflickr.com/1765/29139779108_94633b0bb9.jpg',
-        price: '$15.00',
+        price: '15.00',
         desc: 'Lugger no prey, no pay crack Jennys tea cup draft ahoy Nelsons folly gally scuppers interloper transom. Tackle aft flogging run a rig bilge scuppers gally knave bring a spring upon her cable long boat. Sutler chandler Cat oNine tails Arr fathom cackle fruit jury mast Barbary Coast fire in the hole Yellow Jack.',
         author: {
             id: "588c2e092403d111454fff82",
@@ -26,7 +26,7 @@ const data = [
     {
         name: 'Cape Cod Bay',
         img: 'https://farm4.staticflickr.com/3617/3389236883_ef334dc46c.jpg',
-        price: '$21.00',
+        price: '21.00',
         desc: 'Port quarter yo-ho-ho sloop Gold Road plunder bilge Letter of Marque come about brigantine. Walk the plank belaying pin cable gangplank fore fire in the hole no prey, no pay square-rigged bilge rat black spot. Brig Jack Ketch draught maroon Sea Legs doubloon lass jack Brethren of the Coast lee.',
         author: {
             id: "588c2e092403d111454fff90",
